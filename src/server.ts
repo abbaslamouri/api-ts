e79f7a3257deab84a0e8b0d1064176ab696f043b
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-04-25 17:00:13 -0400

    Added mongo db connection

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,16 @@ import colors from 'colors'
 import 'dotenv/config'
 import app from './app'
 
-// const db = (process.env.DB_URL as string).replace('<PASSWORD>', process.env.DB_PASSWORD as string)
-// mongoose
-//   .connect(db, {})
-//   .then(() => {
-//     console.log(colors.magenta.bold(`Database connection succesfull`))
-//   })
-//   .catch((err) => {
-//     console.log(colors.red.bold(`Mongo DB Error ${err}`))
-//     console.log(colors.red.bold(`Mongo DB Error Message ${err.message}`))
-//   })
+const db = process.env.DB_URL as string
+mongoose
+  .connect(db, {})
+  .then(() => {
+    console.log(colors.magenta.bold(`Database connection succesfull`))
+  })
+  .catch((err) => {
+    console.log(colors.red.bold(`Mongo DB Error ${err}`))
+    console.log(colors.red.bold(`Mongo DB Error Message ${err.message}`))
+  })
 
 const port: String = process.env.PORT || '5000'
 app.listen(port, () => {
