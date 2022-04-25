3afa8df4869d1731ded250a7d6b438b988cb3a99
Abbas Lamouri <abbaslamouri@yrlus.com>
2022-04-25 16:11:53 -0400

    M

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ app.use(express.json({ limit: '1000kb' }))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hellssso')
+  res.send('Hello')
 })
 
 export default app
